fcba6d0ffefa689f129f01460465186059a5dd37
user49-div <mohamedyoussef49574@gmail.com>
2025-11-20 17:30:39 +0100

    easy

---
+++ Medium.js
@@ -0,0 +1,3 @@
+import { employees } from "./dataset.js"
+
+
---
+++ easy.js
@@ -0,0 +1,72 @@
+import { employees } from "./dataset.js";
+
+
+
+// easy1
+
+
+// function countEnglishSpeakingEmployees (){
+//       return employees[0].firstName;
+// }
+
+// console.log(countEnglishSpeakingEmployees())
+
+
+// easy2
+
+
+// function getLastEmployeeLastName (){
+//     return employees[employees.length-1].lastName;
+// }
+// console.log(getLastEmployeeLastName())
+
+
+
+// easy 3
+
+// function getEmployeeDepartmentById(){
+//     for(let i = 0 ; i < employees.length ; i++){
+//         if(employees[i].id === 5){
+//             return employees[i].department;
+//         }
+//     }
+// }
+// console.log(getEmployeeDepartmentById())
+
+
+// easy4
+
+// function countEmployees(){
+//     return employees.length
+// }
+// console.log(countEmployees())
+
+
+// easy5
+
+// function hasInactiveEmployees(){
+//     for(let i = 0 ; i < employees.length ; i++){
+//         if(employees[i].isActive === false){
+//             return true ;
+//         }
+//     }
+// }
+
+// console.log(hasInactiveEmployees())
+
+// easy6
+
+// function calculateAverageAge(){
+//     let x = 0
+//     for(let i = 0 ; i < employees.length ; i++){
+//         x += (employees[i].age)/employees.length
+//     }
+//     return x ;
+// }
+
+// console.log(calculateAverageAge())
+
+
+// easy7
+
+function 
